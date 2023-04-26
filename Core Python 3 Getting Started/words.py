4821ce6938b98b0d2d06d7401e67ff773c1b4be1
Sean Murray <seanmurray23@gmail.com>
2023-04-26 13:41:02 -0400

    working through video

--- Core Python 3 Getting Started/words.py
+++ Core Python 3 Getting Started/words.py
@@ -1,9 +1,24 @@
 from urllib.request import urlopen
 
-story = urlopen('https://sixty-north.com/c/t.txt')
-story_words = []
-for line in story:
-    line_words = line.decode('utf-8').split()
-    for word in line_words:
-        story_words.append(word)
-story.close()
+def fetch_words():
+    story = urlopen('https://sixty-north.com/c/t.txt')
+    story_words = []
+    for line in story:
+        line_words = line.decode('utf-8').split()
+        for word in line_words:
+            story_words.append(word)
+    story.close()
+    return story_words
+
+def print_words(story_words):
+    for word in story_words:
+        print(word)
+
+def main():
+    words = fetwords()
+    print_words(words)
+
+
+
+if __name__ == '__name__':
+    main()
