05a3ae6d7e02b7a72a7a15c720a89fb7cf22342d
SeanMurray23 <seanmurray23@gmail.com>
2023-05-24 10:53:23 -0400

    section for course completed on the the next section!

--- Core Python 3 Getting Started/demo_reader/compressed/gzipped.py
+++ Core Python 3 Getting Started/demo_reader/compressed/gzipped.py
@@ -1,9 +1,8 @@
 import gzip
-import sys
+
+from .. import util
 
 opener = gzip.open
 
 if __name__ == '__main__':
-    f = gzip.open(sys.argv[1], mode='wt')
-    f.write(' '.join(sys.argv[2:]))
-    f.close()
+    writer.main(opener)
