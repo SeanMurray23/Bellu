fe6e2c41d454f87f65dc9430c6797c4988e3d523
SeanMurray23 <seanmurray23@gmail.com>
2023-05-31 10:57:45 -0400

    next video complete

--- Classes and Objects/classes.py
+++ Classes and Objects/classes.py
@@ -5,10 +5,15 @@ class Employee:
         self.age = age
         self.position = position
         self.salary = salary
+    
+    def increase_salary(self, percent):
+        self.salary += self.salary * (percent/100)
         
+    def info(self):
+        print(f"{self.name} is {self.age} years old. Employee is a {self.position} with the salary of ${self.salary}")
 
 employee1 = Employee("Jin", 38 , "developer", 1200)
 employee2 = Employee("Laura", 44, "tester", 1000)
 
-print(employee1.name)
-print(employee2.name)
+Employee.increase_salary(employee2, 20)
+employee2.info()
