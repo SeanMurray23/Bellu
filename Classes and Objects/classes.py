2296094850b6df1cdb852238ed65f71a0b180e63
SeanMurray23 <seanmurray23@gmail.com>
2023-05-31 10:41:14 -0400

    Started module 3, first video done

---
+++ Classes and Objects/classes.py
@@ -0,0 +1,12 @@
+
+class Employee:
+    def __init__(self):
+        self.__dict__['name'] = "Ji-soon"
+        self.__dict__['age'] = "38"
+        self.__dict__['position'] = "developer"
+        self.__dict__['salary'] = "1200"
+        
+
+e = Employee()
+
+print(e.name)
