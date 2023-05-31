657d90ab143007a4244c6b9203d7f7f3a734ea4c
SeanMurray23 <seanmurray23@gmail.com>
2023-05-31 11:10:30 -0400

    str and repr video done

--- Classes and Objects/classes.py
+++ Classes and Objects/classes.py
@@ -11,8 +11,13 @@ class Employee:
         
     def __str__(self):
         return f"{self.name} is {self.age} years old. Employee is a {self.position} with the salary of ${self.salary}"
-
     
+    def __repr__(self):
+        return (
+            f"Employee("
+            f"{repr(self.name)}, {repr(self.age)}, "
+            f"{repr(self.position)}, {repr(self.salary)})"
+        )
 
 employee1 = Employee("Jin", 38 , "developer", 1200)
 employee2 = Employee("Laura", 44, "tester", 1000)
@@ -20,3 +25,5 @@ employee2 = Employee("Laura", 44, "tester", 1000)
 Employee.increase_salary(employee2, 20)
 
 print(employee2)
+print(str(employee2))
+print(repr(employee2))
