153a9f665b2c0b89306063a82bdef6f416698d47
SeanMurray23 <seanmurray23@gmail.com>
2023-06-15 10:06:20 -0400

    depth first pre order video done

--- Classes and Objects/iterable.py
+++ Classes and Objects/iterable.py
@@ -1,10 +1,14 @@
-def _if_perfect_length(sequence):
+def _is_perfect_length(sequence):
     """true if sequence has length of 2n -1 otherwise false"""
     n = len(sequence)
     return ((n+1) & n==0) and (n != 1)
+
+
 class LevelOrderIterator:
     
     def __init__(self,sequence):
+        if not _is_perfect_length(sequence):
+            raise ValueError(f"Sequence of {len(sequence)} does not rep a binary tree of 2-1")
         self._sequence = sequence #iterable passed to it
         self._index = 0 #sets starting point
         
@@ -17,3 +21,34 @@ class LevelOrderIterator:
     
     def __iter__(self): 
         return self # returns self so it can be used anywhere
+    
+def _left_child(index):
+    return 2 * index +1
+
+def _right_child(index):
+    return 2 * index +2
+
+
+class PreORderIterator:
+    def __init__(self,sequence):
+        if not _is_perfect_length(sequence):
+            raise ValueError(f"Sequence of {len(sequence)} does not rep a binary tree of 2-1")
+        self._sequence = sequence #iterable passed to it
+        self._stack = [0] #sets starting point
+        
+    def __next__(self):
+        if len(self._stack) == 0: # checks to see if its at the end of the table
+            raise StopIteration
+        index = self._stack.pop() #pop off the top item of the stack
+        result = self._sequence[index] #
+
+        #preorder push: Push right child first so left child is popped and processed first, last in first out
+        right_child_index = _right_child(index)
+        if right_child_index < len(self._sequence):
+            self._stack.append(right_child_index)
+            
+        _left_child_index = _left_child(index)
+        if _left_child_index < len(self._sequence):
+            self._stack.append(_left_child_index)
+            
+        return result
