a9413144918828556d9c79fc432d9c9c083edb09
SeanMurray23 <seanmurray23@gmail.com>
2023-06-15 10:36:11 -0400

    inorder iterator done

--- Classes and Objects/iterable.py
+++ Classes and Objects/iterable.py
@@ -29,7 +29,7 @@ def _right_child(index):
     return 2 * index +2
 
 
-class PreORderIterator:
+class PreOrderIterator:
     def __init__(self,sequence):
         if not _is_perfect_length(sequence):
             raise ValueError(f"Sequence of {len(sequence)} does not rep a binary tree of 2-1")
@@ -52,3 +52,31 @@ class PreORderIterator:
             self._stack.append(_left_child_index)
             
         return result
+
+class InOrderIterator:
+    def __init__(self,sequence):
+        if not _is_perfect_length(sequence):
+            raise ValueError(f"Sequence of {len(sequence)} does not rep a binary tree of 2-1")
+        self._sequence = sequence #iterable passed to it
+        self._stack = [] #sets starting point
+        self._index = 0
+        
+    def __next__(self):
+        if (len(self._stack ==0)) and (self._index>= len(self._sequence)):
+            raise StopIteration
+        
+        #Push left children onto the stack while possible
+        while self._index < len(self._sequence):
+            self._stack.append(self._index)
+            self._index = _left_child(self._index)
+            
+        #pop froim stack and process beforeing moving to the right child
+        
+        index = self._stack.pop()
+        result = self._sequence[index]
+        self._index = _right_child(index)
+        return result
+    
+    def __iter__(self):
+        return self
+        
