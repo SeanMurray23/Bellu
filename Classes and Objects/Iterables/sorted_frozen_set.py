1419555a29ccafd54cffb318c72191e7c3bae519
Sean <sean.murray@bell.ca>
2023-06-19 10:22:28 -0400

    iterable protocol

--- Classes and Objects/Iterables/sorted_frozen_set.py
+++ Classes and Objects/Iterables/sorted_frozen_set.py
@@ -1,7 +1,10 @@
 class SortedFrozenSet:
 
     def __init__(self, items=None):
-        self._items = list(items) if (items is not None) else list()
+        self._items = set(items) if (items is not None) else set()
 
     def __contains__(self, item):
         return item in self._items
+    
+    def __len__(self):
+        return len(self._items)
