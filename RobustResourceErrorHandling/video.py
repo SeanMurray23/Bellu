d00baf7afeb1e424a061d5a3b7d684afb50228d3
SeanMurray23 <seanmurray23@gmail.com>
2023-07-06 10:08:43 -0400

    update to context manager

--- RobustResourceErrorHandling/video.py
+++ RobustResourceErrorHandling/video.py
@@ -1,6 +1,9 @@
 class LoggingContextManager:
     def __enter__(self):
-        return self
+        print("LogginContextManager.__enter__()")
+        return "Your in a with block!"
     
     def __exit__(self,exc_type,exc_val,exc_tb):
+        print('LoggingContxtManager.__exit__({},{},{})'.format(exc_type,exc_val,exc_tb))
         return
+    
